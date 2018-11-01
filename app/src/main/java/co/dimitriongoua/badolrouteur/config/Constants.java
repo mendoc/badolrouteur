1be88e1986d53e7d5c60c58cbbf63d545ae721b5
mendoc <ongouadimitri5@gmail.com>
2018-11-01 12:43:55 +0100

    Suppresion du lien de téléchargement dans le SMS

--- app/src/main/java/co/dimitriongoua/badolrouteur/config/Constants.java
+++ app/src/main/java/co/dimitriongoua/badolrouteur/config/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
     public static final String SMS_PREFIX = "PP";
     public static final String SMS_RECU = "recu";
     public static final String SMS_ADDRESS = "AirtelMoney";
-    public static final String URL_APP = "http://bit.ly/2EzoWvl";
+    public static final String URL_APP = "";
 }
